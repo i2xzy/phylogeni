08815b98fddbf61fa01ce365c77ffb25f78f3467
ch1ru <caleb1@criptext.com>
2025-02-01 18:04:34 +0000

    style(pages): update style of clade difference dialog

--- src/app/(user)/clade/[id]/page.tsx
+++ src/app/(user)/clade/[id]/page.tsx
@@ -102,7 +102,7 @@ export default async function CladePage({ params }: PageProps<'/clade/[id]'>) {
               <RiEdit2Fill size="1.4em" /> Edit
             </NavLink>
             <NavLink href={`/clade/${data.id}/revisions`}>
-              <RiHistoryFill size="1.4em" /> Changelog
+              <RiHistoryFill size="1.4em" /> History
             </NavLink>
           </Stack>
           <DataListRoot orientation="horizontal" gap="2">
